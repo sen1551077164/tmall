a2f612e78bb7880aaae87ddbb7a51a4c50a21d95
senmiao <namelzh@foxmail.com>
2021-05-22 23:51:16 +0800

    给接口添加了描述信息

--- src/main/java/com/senmiao/controller/CategoryController.java
+++ src/main/java/com/senmiao/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.senmiao.controller;
 import com.senmiao.service.GoodsCategoryService;
 import com.senmiao.util.RetResult;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ public class CategoryController {
     @Autowired
     private GoodsCategoryService goodsCategoryService;
 
+    @ApiOperation(value = "这是一个获取所有分类的接口",notes = "这是一个获取所有分类的接口")
     @PostMapping("/getCategorys")
     @ResponseBody
     public Object getCategorys(){
--- src/main/java/com/senmiao/controller/EstimationController.java
+++ src/main/java/com/senmiao/controller/EstimationController.java
@@ -2,6 +2,9 @@ package com.senmiao.controller;
 
 import com.senmiao.service.EstimationService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +20,13 @@ public class EstimationController {
     @Autowired
     private EstimationService estimationService;
 
+
+    @ApiOperation(value = "通过商品id获取商品的评价",notes = "通过商品id获取商品的评价")
+    @ApiImplicitParams(
+            {@ApiImplicitParam(name = "id",value = "商品id"),
+                    @ApiImplicitParam(name = "page",value = "页码数"),
+                    @ApiImplicitParam(name = "size",value = "每页的大小")}
+    )
     @PostMapping("/getEstimationByGoodsId/{id}/{page}/{size}")
     public Object getEstimationByGoodsId(@PathVariable Integer id,@PathVariable Integer page,@PathVariable Integer size){
         return  estimationService.getEstimationByGoodsId(id,page,size);
