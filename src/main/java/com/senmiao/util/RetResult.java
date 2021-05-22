c25fd98b3bdfc2a58f4e072aed91fb7bb86deafd
senmiao <namelzh@foxmail.com>
2021-05-22 21:24:36 +0800

    优化了RetResult类代码

--- src/main/java/com/senmiao/util/RetResult.java
+++ src/main/java/com/senmiao/util/RetResult.java
@@ -1,23 +1,14 @@
 package com.senmiao.util;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 //返回结果
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RetResult<T> {
     int code;
     T data;
     String msg;
-
-    public RetResult(int code,String msg, T data) {
-        this.code = code;
-        this.msg = msg;
-        this.data = data;
-    }
-
-    public RetResult() {
-    }
-
 }
